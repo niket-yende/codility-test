763c4bb6dadc906da505c782fa2f38ff894bc39e
Niket Yende <niket6@gmail.com>
2023-11-08 20:13:13 +0530

    Add solution to find longest special sub-sequence

---
+++ src/other/coffee_io/level5/LongestSequence.java
@@ -0,0 +1,50 @@
+package other.coffee_io.level5;
+
+import java.util.*;
+
+/**
+ * Program to find the longest special subsequence of the array such that
+ * absolute difference b/w every adjacent element in the sub-sequence is one.
+ */
+public class LongestSequence {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        int ans = solution(a, n);
+        System.out.println(ans);
+    }
+
+    static int solution(int[] a, int n) {
+        List<Integer> longestSubSequence = new ArrayList<>();
+        for (int i = 0; i < a.length; i++) {
+            List<Integer> subSequence = new ArrayList<>();
+            subSequence.add(a[i]);
+            int remainingElements = (a.length - 1) - i;
+            if(remainingElements <= longestSubSequence.size()) {
+                break;
+            }
+            for (int j = i+1; j < a.length; j++) {
+                Integer lastElement = subSequence.get(subSequence.size() - 1);
+                Integer difference = findDifference(lastElement, a[j]);
+                if(difference == 1) {
+                    subSequence.add(a[j]);
+                }
+            }
+
+            if(subSequence.size() > longestSubSequence.size()) {
+                longestSubSequence = subSequence;
+            }
+        }
+
+        return longestSubSequence.size() > 1 ? longestSubSequence.size() : 0;
+    }
+
+    static int findDifference(int x, int y) {
+        int difference = x - y;
+        return Math.abs(difference);
+    }
+}
