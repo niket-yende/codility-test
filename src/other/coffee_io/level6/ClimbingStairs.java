2fac7a56aa09949bd0aa9855e2a9665cd00d866e
Niket Yende <niket6@gmail.com>
2023-11-13 21:16:00 +0530

    Add solution for climbing stairs

---
+++ src/other/coffee_io/level6/ClimbingStairs.java
@@ -0,0 +1,29 @@
+package other.coffee_io.level6;
+
+import java.util.Scanner;
+
+/**
+ * Implement a program to climb stairs using the Dynamic Programming(DP) approach.
+ * Input:
+ * 3
+ * Output:
+ * 3
+ */
+public class ClimbingStairs {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println(solution(n));
+    }
+
+    static int solution(int n) {
+        int dp[] = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++)
+            dp[i] = dp[i - 1] + dp[i - 2];
+        return dp[n];
+    }
+}
